097355064ab799107cac44115fcd7e7d16508653
markbrlaise <markkiguli10@gmail.com>
2023-05-01 18:04:45 +0000

    added an api giving some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,33 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+hbnbModels = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     """hbnbStatus"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    stats = {}
+    for k, v in hbnbModels.items():
+        stats[k] = storage.count(v)
+    return jsonify(stats)
+
+
+if __name__ == "__main__":
+    pass
